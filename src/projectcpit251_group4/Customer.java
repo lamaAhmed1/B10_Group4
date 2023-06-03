99c77f6b1e6da8ffb6ed2d617ab3c01721fcb7b1
wond <wond@DESKTOP-Ziad>
2023-06-03 17:55:50 +0300

    Rana

---
+++ src/projectcpit251_group4/Customer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectcpit251_group4;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Customer {
+    String phone;
+    String userName;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+   
+
+    public Customer(String phone, String userName) {
+        this.phone = phone;
+        this.userName = userName;
+        
+    }
+     public String customerInfo(){
+        
+        return "************ customer information ***************"
+                    + "\n"
+                    + "\n user name:"+this.userName+""
+                     + "\n user phone number "+this.phone;
+        
+    }
+}
