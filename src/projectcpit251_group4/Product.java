f67b6ef1dbc61b97232b06bc036e5d5cc41288b4
Lama <azlama72@gmail.com>
2023-06-04 03:51:49 +0300

    commit lama

---
+++ src/projectcpit251_group4/Product.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectcpit251_group4;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Product {
+       String id;
+       double price;
+       String type;
+   
+    
+     public Product(String id, double price, String type) {
+        this.id = id;
+        this.price = price;
+        this.type = type;
+    }
+    
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+   
+    
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String prodInfo(){
+        
+        return ","+this.id+", "+this.type+", "+this.price;
+        
+    }
+    
+}
